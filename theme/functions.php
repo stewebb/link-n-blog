b660dd340de3c4d6a417eeafe8a3069348be999c
Steven Webb <xiaoancloud@outlook.com>
2024-09-03 17:14:24 +1000

    t

--- theme/functions.php
+++ theme/functions.php
@@ -1,156 +1,29 @@
 <?php
-
-    // Add an admin menu for the settings page
-    /*
-    add_action('admin_menu', function() {
-        add_options_page(
-            'Manage Links',                // Page title
-            'Manage Links',                // Menu title
-            'manage_options',              // Capability
-            'manage_links',               // Menu slug
-            function() {                  // Callback function to display the settings page
-                ?>
-                <div class="wrap">
-                    <h1>Manage Links</h1>
-                    <form method="post" action="options.php">
-                        <?php
-                        settings_fields('link_options_group');
-                        do_settings_sections('manage_links');
-                        submit_button();
-                        ?>
-                    </form>
-                </div>
-                <?php
-            }
-        );
-    });
-    
-    */
-    // Sanitize function for custom_links setting
-    //function sanitize_textarea_field($input) {
-    //    // Convert line breaks to HTML <br> tags
-    //    return wp_kses_post($input);
+    //class DividerControl extends \WP_Customize_Control {
+    //    public $type = 'divider';
+    //
+    //    public function render_content() {
+    //        if ($this->label) {
+    //            echo "<hr />";
+    //            echo "<b><center>" . esc_html($this->label) . "</center></b>";
+    //        }
+    //    }
     //}
 
-    
-    // Add Customizer settings and controls
-    /*
     add_action('customize_register', function($wp_customize) {
-    
-        // Add a new section to the Customizer
-        $wp_customize->add_section('custom_links_section', array(
-            'title'    => __('Custom Links', 'yourtheme'),
-            'priority' => 30, // Order of the section in the Customizer
-        ));
-    
-        // Add a setting for the links
-        $wp_customize->add_setting('custom_links', array(
-            'default'           => array(),
-            'sanitize_callback' => 'sanitize_textarea_field', // Custom sanitization function
-        ));
-    
-        // Add a control for the links setting
-        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'custom_links_control', array(
-            'label'    => __('Add Your Links', 'yourtheme'),
-            'section'  => 'custom_links_section',
-            'settings' => 'custom_links',
-            'type'     => 'textarea', // Use 'textarea' for multiple links input
-        )));
-    });
-    */
-    
-   
-    
-    /*
-    function lnb_admin_register($wp_customize) {
-        // Add a section for custom links
-        $wp_customize->add_section('homepage_links_section', array(
-            'title'    => __('Homepage Links', 'mytheme'),
-            'priority' => 30,
-        ));
-    
-        
-        // Add settings for each link
-        for ($i = 1; $i <= 5; $i++) {
-            $wp_customize->add_setting("link_{$i}_url", array(
-                'default' => '',
-                'sanitize_callback' => 'esc_url_raw',
-            ));
-    
-            $wp_customize->add_control("link_{$i}_url", array(
-                'label'    => __("Link $i URL", 'mytheme'),
-                'section'  => 'homepage_links_section',
-                'type'     => 'url',
-            ));
-    
-            $wp_customize->add_setting("link_{$i}_text", array(
-                'default' => "Link $i",
-                'sanitize_callback' => 'sanitize_text_field',
-            ));
-    
-            $wp_customize->add_control("link_{$i}_text", array(
-                'label'    => __("Link $i Text", 'mytheme'),
-                'section'  => 'homepage_links_section',
-                'type'     => 'text',
-            ));
-        }
-        
-    }
-    
 
-    add_action('customize_register', 'lnb_admin_register');
-    */
-
-    /*
-    add_action('customize_register', function($wp_customize) {
         // Add a section for custom links
         $wp_customize->add_section('homepage_links_section', array(
             'title'    => __('Homepage Links', 'mytheme'),
             'priority' => 30,
         ));
 
-        $i = 1;
-    
-        
-        // Add settings for each link
-        //for ($i = 1; $i <= 5; $i++) {
-            $wp_customize->add_setting("link_{$i}_url", array(
-                'default' => '',
-                'sanitize_callback' => 'esc_url_raw',
-            ));
-    
-            $wp_customize->add_control("link_{$i}_url", array(
-                'label'    => __("Link $i URL", 'mytheme'),
-                'section'  => 'homepage_links_section',
-                'type'     => 'url',
-            ));
-    
-            $wp_customize->add_setting("link_{$i}_text", array(
-                'default' => "Link $i",
-                'sanitize_callback' => 'sanitize_text_field',
-            ));
-    
-            $wp_customize->add_control("link_{$i}_text", array(
-                'label'    => __("Link $i Text", 'mytheme'),
-                'section'  => 'homepage_links_section',
-                'type'     => 'text',
-            ));
-        //}
-    });
-    */
-    add_action('customize_register', function($wp_customize) {
-        // Add a section for custom links
-        $wp_customize->add_section('homepage_links_section', array(
-            'title'    => __('Homepage Links', 'mytheme'),
-            'priority' => 30,
-        ));
-    
         // Add setting for the number of links
         $wp_customize->add_setting('num_links', array(
             'default'           => 3,
             'sanitize_callback' => 'absint',
         ));
-    
+
         $wp_customize->add_control('num_links', array(
             'label'    => __('Number of Links', 'mytheme'),
             'section'  => 'homepage_links_section',
@@ -161,75 +34,104 @@
                 'step' => 1,
             ),
         ));
-    
-        // Generate settings and controls based on the number of links
-        $num_links = get_theme_mod('num_links', 3); // Default to 3 if not set
-    
+
+        // Add a setting for the hero section
+        $wp_customize->add_setting('hero', array(
+            'default'           => '',
+            'sanitize_callback' => 'wp_kses_post',
+        ));
+
+        $wp_customize->add_control('hero', array(
+            'label'         => __('Hero Section', 'mytheme'),
+            'description'   => __('HTML is supported.', 'mytheme'),
+            'section'       => 'homepage_links_section',
+            'type'          => 'textarea',
+        ));
+
+        $num_links = get_theme_mod('num_links', 3);
         for ($i = 1; $i <= $num_links; $i++) {
-            // Add setting for each link URL
-            $wp_customize->add_setting("link_{$i}_url", array(
-                'default'           => '',
-                'sanitize_callback' => 'esc_url_raw',
-            ));
-    
-            $wp_customize->add_control("link_{$i}_url", array(
-                'label'    => __("Link $i URL", 'mytheme'),
-                'section'  => 'homepage_links_section',
-                'type'     => 'url',
-            ));
-    
-            // Add setting for each link text
+
+            // Add a divider
+            //$wp_customize->add_setting('divider', array(
+            //    'default'           => '',
+            //    'sanitize_callback' => 'wp_kses_post',
+            //));
+        
+            //$wp_customize->add_control(new DividerControl($wp_customize, 'divider', array(
+            //    'label'    => __("Link #{$i}", 'mytheme'),
+            //    'section'  => 'homepage_links_section',
+            //)));
+
+            // Text Setting
             $wp_customize->add_setting("link_{$i}_text", array(
                 'default'           => "Link $i",
                 'sanitize_callback' => 'sanitize_text_field',
             ));
-    
+                
             $wp_customize->add_control("link_{$i}_text", array(
-                'label'    => __("Link $i Text", 'mytheme'),
+                'label'    => __("Text #{$i}", 'mytheme'),
                 'section'  => 'homepage_links_section',
                 'type'     => 'text',
             ));
+
+            // External Link Setting
+            $wp_customize->add_setting("link_{$i}_url", array(
+                'default'           => '',
+                'sanitize_callback' => 'esc_url_raw',
+            ));
+
+            $wp_customize->add_control("link_{$i}_url", array(
+                'label'    => __("External Link #{$i}", 'mytheme'),
+                'section'  => 'homepage_links_section',
+                'type'     => 'url',
+            ));
+
+            // Internal Link Setting
+            $wp_customize->add_setting("link_{$i}_internal", array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_text_field',
+            ));
+
+            $wp_customize->add_control("link_{$i}_internal", array(
+                'label'    => __("Internal Blog Link #{$i}", 'mytheme'),
+                'section'  => 'homepage_links_section',
+                'type'     => 'text',
+                'description' => __('Internal link (e.g., /about or /blog)', 'mytheme'),
+            ));
+
+            // Description Setting
+            $wp_customize->add_setting("link_{$i}_description", array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_textarea_field',
+            ));
+
+            $wp_customize->add_control("link_{$i}_description", array(
+                'label'    => __("Description #{$i}", 'mytheme'),
+                'section'  => 'homepage_links_section',
+                'type'     => 'textarea',
+            ));
+
+            // Cover Image Setting
+            $wp_customize->add_setting("link_{$i}_image", array(
+                'default'           => '',
+                'sanitize_callback' => 'esc_url_raw',
+            ));
+
+            $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, "link_{$i}_image", array(
+                'label'    => __("Cover Picture #{$i}", 'mytheme'),
+                'section'  => 'homepage_links_section',
+                'settings' => "link_{$i}_image",
+            )));
         }
     });
+
     
 
-    /*
-    add_action('customize_register', function($wp_customize) {
-        $wp_customize->add_section('homepage_links_section', array(
-            'title'    => __('Homepage Links', 'mytheme'),
-            'priority' => 30,
-        ));
-
-        $wp_customize->add_setting("link_url", array(
-            'default' => '',
-            'sanitize_callback' => 'esc_url_raw',
-        ));
-    });
-    */
-
-
+   
 
     add_action('after_setup_theme', 'mytheme_setup');
 
-    /*
-    function my_theme_settings_page() {
-        echo "qqq";
-    }
-
-    function custom_links_admin()
-    {
-        add_options_page(
-            'Manage Links',                // Page title
-            'Manage Links',                // Menu title
-            'manage_options',              // Capability
-            'manage_links',               // Menu slug
-            'my_theme_settings_page'
-        );    
     
-    }
-
-    add_action('admin_menu', 'custom_links_admin'); 
-    */
 
     function mytheme_setup() {
         add_theme_support('custom-logo', array(
@@ -239,67 +141,10 @@
             'flex-height' => true,
         ));
     }
-
-    //function hello_world_theme_enqueue_styles() {
-    //    wp_enqueue_style('MSX', get_stylesheet_uri());
-    //}
-
     
-    /*
-    // Register Custom Post Type
-    add_action('init', function() {
-        $args = array(
-            'public' => true,
-            'label'  => 'Links',
-            'supports' => array('title'),
-            'menu_position' => 5,
-            'show_in_rest' => true,
-            'has_archive' => false,
-        );
-        register_post_type('link', $args);
-    });
-    
-    // Add Meta Box
-    add_action('add_meta_boxes', function() {
-        add_meta_box(
-            'link_url_meta_box',
-            'Link URL',
-            function($post) {
-                $link_url = get_post_meta($post->ID, 'link_url', true);
-                ?>
-                <label for="link_url">URL:</label>
-                <input type="text" id="link_url" name="link_url" value="<?php echo esc_attr($link_url); ?>" size="30" />
-                <?php
-            },
-            'link',
-            'normal',
-            'high'
-        );
-    });
-    
-    // Save Meta Box Data
-    add_action('save_post', function($post_id) {
-        if (array_key_exists('link_url', $_POST)) {
-            update_post_meta(
-                $post_id,
-                'link_url',
-                sanitize_text_field($_POST['link_url'])
-            );
-        }
-    });
-    */
-
 
     add_action('after_setup_theme', 'mytheme_setup');
 
-
-    //function lnb_enqueue_styles() {
-    //    $dir_uri = get_template_directory_uri();
-    //    wp_enqueue_style('Common', $dir_uri . '/styles/Common.css');
-    //    wp_enqueue_style('Navbar', $dir_uri . '/styles/Navbar.css');
-    //    wp_enqueue_style('Footer', $dir_uri . '/styles/Footer.css');
-    //}
-    //add_action('wp_enqueue_scripts', 'lnb_enqueue_styles');
 
     // Enqueue styles
     add_action('wp_enqueue_scripts', function() {
@@ -307,5 +152,6 @@
         wp_enqueue_style('Common', $dir_uri . '/styles/Common.css');
         wp_enqueue_style('Navbar', $dir_uri . '/styles/Navbar.css');
         wp_enqueue_style('Footer', $dir_uri . '/styles/Footer.css');
+        wp_enqueue_style('LinkItem', $dir_uri . '/styles/LinkItem.css');
     });
 ?>
--- theme/index.php
+++ theme/index.php
@@ -7,16 +7,87 @@
     <title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
     <div id="app">
         <?php require_once(get_template_directory() . "/includes/Navbar.php"); ?>
 
     
+        <!--
+        <div class="event-item disabled" role="button" tabindex="0" aria-disabled="true">
+    
+    <-- Banner --
+    <div class="banner-container">
+        <img src="path-to-banner-image.jpg" alt="Event Name" style="width: 100%; height: auto;" />
+    </div>
 
-        <?php
-// Get the number of links from the Customizer
-$num_links = get_theme_mod('num_links', 3); // Default to 3 if not set
+    <-- Brief info --
+    <div class="px-3 pt-3 pb-1">
+        <h5 class="fw-bold text-secondary" id="eventNameLabel">Event Name</h5>
+
+        <-- Time --
+        <div class="small mb-1">
+            <i class="fa-regular fa-clock fa-fw" aria-label="Time"></i>&nbsp;Event Time
+        </div>
+
+        <-- Location --
+        <div class="small mb-1">
+            <i class="fa-solid fa-location-dot fa-fw" aria-label="Location"></i>&nbsp;Event Location
+        </div>
+
+        <-- Organizer --
+        <div class="small mb-1">
+            <i class="fa-solid fa-users fa-fw" aria-label="Organizer"></i>&nbsp;Event Organizer
+        </div>
+
+        <-- Short description --
+        <div class="small mb-1">
+            <i class="fa-regular fa-comment fa-fw" aria-label="Short description"></i>&nbsp;Short Description
+        </div>
+        
+        <-- Ticket Info --
+        <div class="small mb-1">
+            <-- Ticket info placeholder --
+            Ticket Information
+        </div>
+    </div>
+
+    <-- Like button --
+    <div class="d-flex justify-content-center mb-3">
+        <-- Favorite icon placeholder --
+        <span class="favorite-icon"></span>
+    </div>
+</div>
+
+        -->
+
+        <div class="container-fluid">
+
+            <!-- Hero Section -->
+            <div class="p-3 mb-3">
+                <?= get_theme_mod('hero'); ?>
+            </div>
+
+            <div class="col-xl-3 col-lg-4 col-sm-6 menu-col">
+            <?php
+                $num_links = get_theme_mod('num_links', 3);
+                //if ($num_links > 0) {
+                for ($i = 1; $i <= $num_links; $i++) {
+                    $link_text = get_theme_mod("link_{$i}_text", "Link $i");
+                    
+                    echo '<div class="link-item" role="button" tabindex="0" aria-disabled="true">';
+                    echo '  <span class="text-primary fw-bold">' . esc_attr($link_text) . '</span>';
+                    echo '</div>';
+                }
+                //}
+            ?>
+
+            
+            </div>
+        </div>
+<?php
+//$num_links = get_theme_mod('num_links', 3); // Default to 3 if not set
 
 if ($num_links > 0) {
     echo '<ul class="homepage-links">';
@@ -24,10 +95,32 @@ if ($num_links > 0) {
     for ($i = 1; $i <= $num_links; $i++) {
         $link_url = get_theme_mod("link_{$i}_url");
         $link_text = get_theme_mod("link_{$i}_text", "Link $i");
+        $internal_link = get_theme_mod("link_{$i}_internal");
+        $description = get_theme_mod("link_{$i}_description");
+        $image_url = get_theme_mod("link_{$i}_image");
 
-        if (!empty($link_url)) {
-            echo '<li>';
-            echo '<a href="' . esc_url($link_url) . '">' . esc_html($link_text) . '</a>';
+        if (!empty($link_url) || !empty($internal_link)) {
+            echo '<li class="homepage-link-item">';
+
+            // Image
+            if (!empty($image_url)) {
+                echo '<div class="homepage-link-image">';
+                echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($link_text) . '">';
+                echo '</div>';
+            }
+
+            // Link
+            echo '<a href="' . esc_url($link_url) . '"';
+            if (!empty($internal_link)) {
+                echo ' data-internal-link="' . esc_attr($internal_link) . '"';
+            }
+            echo '>' . esc_html($link_text) . '</a>';
+
+            // Description
+            if (!empty($description)) {
+                echo '<p class="homepage-link-description">' . esc_html($description) . '</p>';
+            }
+
             echo '</li>';
         }
     }
@@ -35,6 +128,7 @@ if ($num_links > 0) {
     echo '</ul>';
 }
 ?>
+
 
 
         
