35287470f8908f480e24e661a57faa9e6368f4cb
stewebb <xiaoancloud@outlook.com>
2024-10-24 20:36:10 +1100

    png

--- lnb.php
+++ lnb.php
@@ -1,85 +1,34 @@
 <?php
+/*
+Plugin Name: Link 'n' Blog
+Description: A
+Version: 1.0
+Author: Your Name
+*/
 
-/**
- * Plugin Name: Your Plugin Name
- * Description: A WordPress plugin that renders admin pages using Twig (without Composer).
- * Version: 1.0
- * Author: Your Name
- */
+// Hook to add menu
+add_action('admin_menu', 'hw_add_admin_page');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-use Timber\Timber;
-//require __DIR__ . '/vendor/autoload.php';
-class MyClass
-{
-
-}
-
-echo class_exists('MyClass');
-echo class_exists('Timber');
-
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
-//error_log(class_exists('Timber') ? 'Timber is loaded' : 'Timber is NOT loaded');
-//echo "hello";
-//echo !class_exists( 'Timber');
-//echo "world<br>";
-// Ensure Timber is loaded
-if (!class_exists( 'Timber' ) ) {
-    add_action( 'admin_notices', function() {
-        echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin.</p></div>';
-    } );
-    return;
-}
-
-// Define the directory for Twig templates
-function ccm_plugin_add_template_directory() {
-    Timber::$dirname = array('views');
-}
-add_action('init', 'ccm_plugin_add_template_directory');
-
-// Register the shortcode
-function ccm_custom_block_shortcode($atts) {
-    $args = shortcode_atts([
-        'title' => 'Default Title',
-        'content' => 'This is default content.',
-    ], $atts);
-
-    // Render the Twig template with passed arguments
-    return Timber::compile('block.twig', $args);
-}
-add_shortcode('custom_block', 'ccm_custom_block_shortcode');
-
-// Create an admin menu page
-function ccm_add_admin_page() {
+// Function to add admin page
+function hw_add_admin_page() {
     add_menu_page(
-        'My Custom Plugin',          // Page title
-        'Custom Plugin',             // Menu title
-        'manage_options',            // Capability
-        'ccm_custom_plugin',         // Menu slug
-        'ccm_render_admin_page',     // Callback function to render the page
-        'dashicons-admin-generic',   // Icon
-        25                           // Position in menu
+        "Link 'n' Blog",      // Page title
+        "Link 'n' Blog",             // Menu title
+        'manage_options',          // Capability
+        'hello-world',             // Menu slug
+        'hw_display_admin_page',   // Callback function
+        plugins_url('LNB_Square.svg', __FILE__), // Path to PNG icon
+        '99'                          // Position
     );
 }
-add_action('admin_menu', 'ccm_add_admin_page');
 
-// Render the admin page using Timber
-function ccm_render_admin_page() {
-    // Check user capability
-    if (!current_user_can('manage_options')) {
-        return;
-    }
-
-    // Prepare data for the template
-    $context = Timber::get_context();
-    $context['title'] = 'My Custom Admin Page';
-    $context['description'] = 'This is an admin page rendered with Timber and Twig.';
-
-    // Render the admin page using a Twig template
-    Timber::render('admin-page.twig', $context);
+// Function to display content on the admin page
+function hw_display_admin_page() {
+    echo '<h1>Hello World from the Admin Page!</h1>';
+    echo '<p>This is a simple admin page for the Hello World plugin.</p>';
 }
+
+function hw_custom_admin_styles() {
+    wp_enqueue_style('hw-admin-css', plugins_url('admin-styles.css', __FILE__));
+}
+add_action('admin_enqueue_scripts', 'hw_custom_admin_styles');
