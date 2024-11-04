21c6741b0637311b72e05efcc9d7da7987857942
stewebb <xiaoancloud@outlook.com>
2024-11-04 20:03:12 +1100

    remove old code

--- theme_old/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once get_template_directory() . '/includes/setup.php';
-    require_once get_template_directory() . '/includes/template.php';
-    require_once get_template_directory() . '/includes/customizer.php';
-?>
