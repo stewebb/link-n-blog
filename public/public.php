60a40492e0e2e8ab5f8241972ff87f6dd71b41f2
stewebb <xiaoancloud@outlook.com>
2024-11-21 21:51:22 +1100

    bug fix + options

--- public/public.php
+++ public/public.php
@@ -36,15 +36,3 @@ add_shortcode('lnb',function ($atts) {
 
 	return link_page($group, $grouped_links);
 });
-
-/*
-add_action('wp', function() {
-	$current_post = get_post();
-
-	if ($current_post && has_shortcode($current_post->post_content, 'lnb')) {
-		echo "Shortcode is loaded!";
-	} else {
-		echo "Shortcode is not loaded.";
-	}
-});
-*/
